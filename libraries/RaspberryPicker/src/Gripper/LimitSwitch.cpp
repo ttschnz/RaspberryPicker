6a9d793bce0ba983b8710984f4434978bf26e33f
Tim T. <ttschnz@protonmail.com>
2025-11-12 14:18:41 +0100

    switch from pullup to default

--- libraries/RaspberryPicker/src/Gripper/LimitSwitch.cpp
+++ libraries/RaspberryPicker/src/Gripper/LimitSwitch.cpp
@@ -3,11 +3,9 @@
 
 LimitSwitch::LimitSwitch(int pin){
     this->pin = pin;
-    pinMode(this->pin, INPUT_PULLUP);
+    pinMode(this->pin, INPUT);
 }
 
 bool LimitSwitch::is_touching(){
-    // we use internal pullup resistor and connect pin to gnd via switch. 
-    // if the switch is closed, we have a LOW reading since we are grounded
-    return digitalRead(this->pin) == LOW;
+    return digitalRead(this->pin) == HIGH;
 }
