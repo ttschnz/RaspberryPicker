7de23575552c3e67a724b2d79f6910923c8acef6
Tim T. <ttschnz@protonmail.com>
2025-11-12 11:58:19 +0100

    add sketches

---
+++ sketches/pressure_sensor/pressure_sensor.ino
@@ -0,0 +1,29 @@
+#include <Arduino.h>
+// data structure for storing colors
+
+
+#define SENSOR A5
+
+float measure_val();
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial)
+    ;
+
+  pinMode(SENSOR, INPUT);
+
+}
+
+
+void loop() {
+  float value = measure_val();
+  Serial.println(
+    (String) 
+    "Value:"  + value
+  );
+}
+
+float measure_val() {
+  return analogRead(SENSOR);
+}
